ce99b4192d160fc064962e2e30390103527ab399
Frederic <bui.frederic.pro@gmail.com>
2022-01-18 12:03:24 +0100

    création de la page de gestion de profil

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\User1Type;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/profil")
+ */
+class ProfilController extends AbstractController
+{
+    
+    /**
+     * @Route("/", name="profil_show", methods={"GET"})
+     */
+    public function show(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('profil/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/edit", name="profil_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(User1Type::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profil_show', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profil/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+}
