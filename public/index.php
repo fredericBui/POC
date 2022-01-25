6d1e1186fab9d5be321e556e350b53496b95978d
Frederic <bui.frederic.pro@gmail.com>
2022-01-25 11:57:34 +0100

    Ajout de front

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
+// Appel de la classe noyau de symfony
 use App\Kernel;
 
+// Cette ligne permet d'utiliser l'auto-loader
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
