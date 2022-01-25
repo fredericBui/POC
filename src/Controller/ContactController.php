158a60bd45d81c6017bd03e91915a863fc04f96c
Frederic <bui.frederic.pro@gmail.com>
2022-01-25 18:50:22 +0100

    mailerService

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +14,24 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, MailerService $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->render('contact/success.html.twig');
+
+            $data=$form->getData();
+            $email=$data['email'];
+            $object=$data['object'];
+            $message=$data['message'];
+            // Mail to admin
+            $mailer->sendEmail($email, "admin@gmail.com", $object, "emails/contact.html.twig", ['inputMessage'=>$message]);
+            // Mail to user
+            $mailer->sendEmail('admin@gmail.com', $email, "Thank you !", "emails/automatic.html.twig", []);
+            return $this->render('contact/success.html.twig',[
+                'email' => $email
+            ]);
         }
 
         return $this->renderForm('contact/index.html.twig', [
