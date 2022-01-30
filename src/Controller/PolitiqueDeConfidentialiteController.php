776e7afe8633fa91e02d83c20ed490d7f8704efa
Frederic <bui.frederic.pro@gmail.com>
2022-01-30 18:27:20 +0100

    Politique de confidentialité et mentions légales

---
+++ src/Controller/PolitiqueDeConfidentialiteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PolitiqueDeConfidentialiteController extends AbstractController
+{
+    /**
+     * @Route("/politique_de_confidentialite", name="politique_de_confidentialite")
+     */
+    public function index(): Response
+    {
+        return $this->render('politique_de_confidentialite/index.html.twig', [
+            'controller_name' => 'PolitiqueDeConfidentialiteController',
+        ]);
+    }
+}
