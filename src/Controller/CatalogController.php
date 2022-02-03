4bdf86eaed7800804d52e8b59b0b54a427183bd0
Frederic <bui.frederic.pro@gmail.com>
2022-02-03 20:59:33 +0100

    front end and simplify search function

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -17,12 +17,21 @@ class CatalogController extends AbstractController
     /**
      * @Route("/", name="catalog_index", methods={"GET"})
      */
-    public function index(PocRepository $pocRepository): Response
+    public function index( Request $request, PocRepository $pocRepository): Response
     {
-        return $this->render('catalog/index.html.twig', [
-            // On retrouve les mêmes méthodes que j'expliquerais dans les repository
-            'pocs' => $pocRepository->findAll(),
-        ]);
+        $data = $request->query->get('keyword');
+
+        if($data){
+            return $this->render('catalog/index.html.twig', [
+                //Recherche par mot clé
+                'pocs' => $pocRepository->findByText($data)]);    
+        }else{
+            return $this->render('catalog/index.html.twig', [
+                // On retrouve les mêmes méthodes que j'expliquerais dans les repository
+                'pocs' => $pocRepository->findAll(),
+            ]);
+        }
+        
     }
    
     /**
@@ -33,19 +42,6 @@ class CatalogController extends AbstractController
         return $this->render('catalog/show.html.twig', [
             'poc' => $poc,
         ]);
-    }
-
-    /**
-     * @Route("/search", name="catalog_search", methods={"GET"})
-     */
-    public function search(PocRepository $pocRepository, Request $request): Response
-    {
-        $data = $request->query->get('keyword');
-        //$data = $request->query;
-        //dd($data);
-        return $this->render('catalog/index.html.twig', [
-            //Recherche par mot clé
-            'pocs' => $pocRepository->findByText($data)]);
     }
 
 }
